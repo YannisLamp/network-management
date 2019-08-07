af620fa68f23db24b5fb79ae58fb81ef753468f9
yannis-ubuntu <yann.lamprou@gmail.com>
2019-08-07 03:50:17 +0300

    custom topology creation removed

--- net-man-backend/backend.py
+++ net-man-backend/backend.py
@@ -20,68 +20,6 @@ cors = CORS(app)
 
 gnet = None
 
-class MinimalTopo( Topo ):
-    "Minimal topology with a single switch and two hosts"
- 
-    def build( self ):
-        # Create two hosts.
-        h1 = self.addHost( 'h1' )
-        h2 = self.addHost( 'h2' )
-
-        h3 = self.addHost( 'h3' )
-        h4 = self.addHost( 'h4' )
- 
-        # Create a switch
-        s1 = self.addSwitch( 's1' )
-        s2 = self.addSwitch( 's2' )
-        s3 = self.addSwitch( 's3' )
-
- 
-        # Add links between the switch and each host
-        self.addLink( s1, h1 )
-        self.addLink( s1, h2 )
-
-        self.addLink( s2, h3 )
-        self.addLink( s2, h4 )
-
-        # Switch to link swicthes
-        self.addLink( s3, s1 )
-        self.addLink( s3, s2 )
-
-
-def runMinimalTopo():
-    "Bootstrap a Mininet network using the Minimal Topology"
- 
-    # Create an instance of our topology
-    topo = MinimalTopo()
-    
-    switch = partial( OVSSwitch, protocols='OpenFlow13' )
-
-    # Create a network based on the topology using OVS and controlled by
-    # a remote controller.
-    net = Mininet(
-        topo=topo,
-        controller=lambda name: RemoteController( name, ip='127.0.0.1' ),
-        switch=switch,
-        autoSetMacs=True,
-    )
- 
-    # Actually start the network
-    net.start()
- 
-    # Drop the user in to a CLI so user can run commands.
-    #CLI( net )
- 
-    # After the user exits the CLI, shutdown the network.
-    #net.stop()
- 
-
-    # We need this to be global
-    global gnet
-    gnet = net
-
-    print 'created mini'
-
 
 def createNet(controllerIp, controllerPort, topoType, 
         switchType, nodesPerSwitch, switchNum, mac):
@@ -130,9 +68,9 @@ def createNet(controllerIp, controllerPort, topoType,
     gnet = net
 
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+# @app.route('/')
+# def index():
+#     return "Hello, World!"
 
 
 @app.route('/network', methods=['POST'])
@@ -153,12 +91,8 @@ def create_network():
     switchType = request.json.get('switchType')
     switchNum = int( request.json.get('switchNum') )
     nodesPerSwitch = int( request.json.get('nodesPerSwitch') )
-    
-    defaultTopo = request.json.get('defaultTopo')
-    
-    # Create network
-    #runMinimalTopo()
 
+    # Create Network
     createNet(ip, port, topoType, switchType, nodesPerSwitch, 
             switchNum, mac)
 
