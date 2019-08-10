4d8ecd56c2401259ec810ed152831d3a42efe7fd
JackaLakos <giannispapad96@gmail.com>
2019-08-10 21:46:05 +0300

    Changed statisticsApp topologyGraph

--- net-man-app/src/containers/StatisticsApp/statisticsApp.js
+++ net-man-app/src/containers/StatisticsApp/statisticsApp.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Table } from 'reactstrap';
-
-
 import { Link } from 'react-router-dom';
 
 //import styles from './statisticsApp.module.css';
@@ -9,33 +7,136 @@ import { openDaylightApi } from '../../services/openDaylightApi';
 
 import TopologyGraph from '../TopologyGraph/topologyGraph';
 import produce from 'immer';
+import pcSVG from '../../assets/svg/pcIcon.svg';
+import switchSVG from '../../assets/svg/hub.svg';
 
 
 class StatisticsApp extends Component {
 
     state = {
-        selectedNodeInfo: null
+        isLoading: true,
+        statistics: null,
+        selectedNodeId: null,
+        selectedLinkId: null,
+        linksInfo: null,
+        nodesInfo: null
     }
 
-    nodeClickedHandler = (nodeInfo) => {
-        
+    componentDidMount() {
+        openDaylightApi.getTopology()
+            .then(data => {
+                console.log('openDaylight data:');
+                console.log(data['network-topology'].topology);
+                this.setState(
+                    produce(draft => {
+                        draft.statistics = data['network-topology'].topology;
+                        // draft.isLoading = false;
+                    })
+                );
+
+                this.setGraphData();
+            });
     }
 
+    setGraphData = () => {
+        let retNodes = [];
+        let retLinks = [];
+
+        console.log("------>MAKING DATA<-----------");
+
+        // Can handle many topologies
+        for (let topology of this.state.statistics) {
+            // Nodes
+            for (let node of topology.node) {
+                // Check if node is a swicth or a host
+                // Termination points have themselves as a termination point
+                console.log(node);
+                console.log("------------");
+                let color = 'green';
+                let svgIcon = pcSVG;
+                let switchNames = new Set(); ;
+                if (node['termination-point'][0]['tp-id'] !== node['node-id']) {
+                    color = 'red';
+                    svgIcon = switchSVG;
+                    // Save switch names
+                    //switchNames.add(node['node-id']);
+                }
+
+                const currNode = {
+                    id: node['node-id'],
+                    color: color,
+                    svg: svgIcon,
+                }
+                retNodes.push(currNode);
+            }
+            // Then links
+            for (let link of topology.link) {
+                const currLink = {
+                    source: link.source['source-node'],
+                    target: link.destination['dest-node'], 
+                }
+                retLinks.push(currLink);
+            }
+        } 
+
+
+        this.setState(
+            produce(draft => {
+                draft.nodesInfo = retNodes;
+                draft.linksInfo = retLinks;
+                draft.isLoading = false;
+            })
+        );
+    }
+
+
+    nodeClickedHandler = (nodeId) => {
+        alert(`node clicked ${nodeId}`);
+        this.setState(
+            produce(draft => {
+                draft.selectedNodeId = nodeId;
+            })
+        );
+    }
+
+    linkClickedHandler = (linkId) => {
+        alert(`link clicked ${linkId}`);
+
+        this.setState(
+            produce(draft => {
+                draft.selectedLinkId = linkId;
+            })
+        );
+    }
+
+    graphClickedHandler = () => {
+        alert("graph background clicked");
+    }
 
     render () {
 
+        // alert("rendering app")
+
+
         return (
             <Container fluid>
-                <Row 
-                //className="border"
-                >
-                    <Col sm="7" className="border">
+                {this.state.isLoading ?
+                    "is loading"
+                :
+
+                <>
+                <Row className="border h-75">
+                    <Col sm="9" className="border">
                         <TopologyGraph
                             nodeClickedHandler={this.nodeClickedHandler}
-                        />
+                            linkClickedHandler={this.linkClickedHandler}
+                            graphClickedHandler={this.graphClickedHandler}
+                            nodes={this.state.nodesInfo}
+                            links={this.state.linksInfo}
+                       />
                     </Col>
 
-                    <Col sm="5" className="border">
+                    <Col sm="3" className="border">
                         <Table responsive>
                             <thead>
                             <tr>
@@ -81,6 +182,12 @@ class StatisticsApp extends Component {
                         
                     </Col>
                 </Row>
+
+                <Row className="border">
+wswswswswsw
+                </Row>
+                </>
+                }
 
             </Container>
         )
--- net-man-app/src/containers/TopologyGraph/topologyGraph.js
+++ net-man-app/src/containers/TopologyGraph/topologyGraph.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Button, Row, Col, Form, FormGroup, Input, Label, Spinner, Card, CardHeader, CardText, CardBody, CardFooter,
-    CardTitle, CardSubtitle, Table } from 'reactstrap';
+
 import { withRouter } from 'react-router-dom';
 import produce from 'immer';
 
 import styles from './topologyGraph.module.css';
-
 import { Graph } from 'react-d3-graph';
 
-import { openDaylightApi } from '../../services/openDaylightApi';
-// import pcSVG from '../../assets/svg/hostComputer.svg';
-import pcSVG from '../../assets/svg/pcIcon.svg';
-//import pcSVG from '../../assets/svg/host80s.svg';
-import switchSVG from '../../assets/svg/hub.svg';
-
-
-const nodesInfo = {
-    "Harry": {
-        name: 'Harry', sur: "pap"
-    }
-}
-
-
-// const linksInfo = {
-//     ""
-// }
 
 const myConfig = {
     nodeHighlightBehavior: true,
@@ -47,109 +28,17 @@ const myConfig = {
     }
 };
 
-// const data = {
-//     nodes: [{ id: "Harry",    color:'red'}, { id: 'Sally' }, { id: 'Alice' }],
-//     links: [{ source: 'Harry', target: 'Sally', la: "1" }, { source: 'Harry', target: 'Alice' }]
-// };
-
 
 class TopologyGraph extends Component {
 
-    state = {
-        isLoading: true,
-        graphData: null,
-    }
-
-    
-
-    componentDidMount() {
-        openDaylightApi.getTopology()
-            .then(data => {
-                console.log('openDaylight data:');
-                console.log(data['network-topology'].topology);
-                this.setState(
-                    produce(draft => {
-                        draft.graphData = data['network-topology'].topology;
-                        draft.isLoading = false;
-                    })
-                );
-            });
-
-        // openDaylightApi.getNodes()
-        //     .then(data => {
-        //         console.log('node data:');
-        //         console.log(data);
-
-        //     });
-    }
-
-    makeData = () => {
-        let retNodes = [];
-        let retLinks = [];
-
-        // Can handle many topologies
-        for (let topology of this.state.graphData) {
-            // Nodes
-            for (let node of topology.node) {
-                // Check if node is a swicth or a host
-                // Termination points have themselves as a termination point
-                let color = 'green';
-                let svgIcon = pcSVG;
-                let switchNames = new Set(); ;
-                if (node['termination-point'][0]['tp-id'] !== node['node-id']) {
-                    color = 'red';
-                    svgIcon = switchSVG;
-                    // Save switch names
-                    //switchNames.add(node['node-id']);
-                }
-
-                const currNode = {
-                    id: node['node-id'],
-                    color: color,
-                    svg: svgIcon,
-                }
-                retNodes.push(currNode);
-            }
-            // Then links
-            for (let link of topology.link) {
-                const currLink = {
-                    source: link.source['source-node'],
-                    target: link.destination['dest-node'], 
-                }
-                retLinks.push(currLink);
-            }
-        } 
-
-        return { nodes: retNodes, links: retLinks };
-    }
-
-
-
-
     // graph event callbacks
-    onClickGraph = function() {
-        window.alert(`Clicked the graph background`);
+    onClickGraph = () => {
+        this.props.graphClickedHandler();
     };
     
-    onClickNode = function(nodeId) {
-        window.alert(`Clicked node ${nodeId}`);
-        if (this.props.statisticsMode)
-        { //just show extended info
-
-        }
-        else
-        { //pick two nodes to calc their shortest path
-            
-        }
+    onClickNode = (nodeId) => {
+        this.props.nodeClickedHandler(nodeId);
     };
-    
-    // onDoubleClickNode = function(nodeId) {
-    //     window.alert(`Double clicked node ${nodeId}`);
-    // };
-    
-    // onRightClickNode = function(event, nodeId) {
-    //     window.alert(`Right clicked node ${nodeId}`);
-    // };
     
     // onMouseOverNode = function(nodeId) {
     //     window.alert(`Mouse over node ${nodeId}`);
@@ -159,13 +48,9 @@ class TopologyGraph extends Component {
     //     window.alert(`Mouse out node ${nodeId}`);
     // };
     
-    onClickLink = function(source, target, la) {
-        window.alert(`Clicked link between ${source} and ${target} with  special info: ${la}`);
+    onClickLink = (source, target) => {
+        this.props.linkClickedHandler(source + "-" + target);
     }
-    
-    // onRightClickLink = function(event, source, target) {
-    //     window.alert(`Right clicked link between ${source} and ${target}`);
-    // };
     
     // onMouseOverLink = function(source, target) {
     //     window.alert(`Mouse over in link between ${source} and ${target}`);
@@ -175,38 +60,27 @@ class TopologyGraph extends Component {
     //     window.alert(`Mouse out link between ${source} and ${target}`);
     // };
 
-
     render(){
-        if (this.state.isLoading) {
-            return null;
+        // alert("graph is rendering")
+        const graphData = {
+            nodes: this.props.nodes,
+            links: this.props.links
         }
-        else { 
-            const data = this.makeData();
-            console.log('this is the data');
-            console.log(data);
-            return (   
-                <Graph
-                    id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-                    data={data}
-                    config={myConfig}
-                    onClickNode={this.onClickNode}
-                    onRightClickNode={this.onRightClickNode}
-                    onClickGraph={this.onClickGraph}
-                    onClickLink={this.onClickLink}
-                    onRightClickLink={this.onRightClickLink}
-                    onMouseOverNode={this.onMouseOverNode}
-                    onMouseOutNode={this.onMouseOutNode}
-                    onMouseOverLink={this.onMouseOverLink}
-                    onMouseOutLink={this.onMouseOutLink}
-                    className={styles.graphBox}
-                />
-            );
-        }
+        
+        return (   
+            <Graph
+                id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+                data={graphData}
+                config={myConfig}
+                onClickNode={this.onClickNode}
+                onClickGraph={this.onClickGraph}
+                onClickLink={this.onClickLink}
+                className={styles.graphBox}
+            />
+        );
+        
     }
 
-
-
 }
-
 
 export default withRouter(TopologyGraph);
