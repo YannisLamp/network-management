c25fededfd025a8e1300ae0aff3c4edc11a0c8d4
dimitris <jimgangas@gmail.com>
2019-08-13 18:20:27 +0300

    Moving topology info storing from statistics to menu page -not completed

--- net-man-app/src/containers/ApplicationMenu/applicationMenu.js
+++ net-man-app/src/containers/ApplicationMenu/applicationMenu.js
@@ -3,21 +3,122 @@ import { Container, Row, Col } from 'reactstrap';
 import { Button } from 'reactstrap';
 import { Jumbotron } from 'reactstrap';
 
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import styles from './applicationMenu.module.css';
 import { openDaylightApi } from '../../services/openDaylightApi';
 import { networkApi } from '../../services/networkApi';
 import { getODLnodes, getODLlinks } from '../../utilities/ODL_utilities';
+import pcSVG from '../../assets/svg/pcIcon.svg';
+import switchSVG from '../../assets/svg/hub.svg';
+import produce from 'immer';
+
 
 
 class CreateNetwork extends Component {
     
-
-
-    inputChangedHandler = (event, formElemId) => {
-
+    state = {
+        graphNodes: null,
+        graphLinks: null,
+        nodesInfo: null,
+        linksInfo: null
     }
+
+
+    componentDidMount() {
+        alert("did mount");
+        if (this.state.graphNodes)
+        { // graph data have already been retrieved
+            alert("already retrieved")
+            return;
+        }
+
+        alert("go to retrieve data")
+
+        openDaylightApi.getTopology()
+            .then(data => {
+                console.log('openDaylight data:');
+                console.log(data['network-topology'].topology);
+                
+                this.setGraphData(data['network-topology'].topology);
+            });
+    }
+
+    setGraphData = (statistics) => {
+        let retNodes = [];
+        let retLinks = [];
+        let retNodesInfo = {};
+
+        console.log("------>MAKING DATA<-----------");
+
+        // Can handle many topologies
+        for (let topology of statistics) {
+            // Nodes
+            for (let node of topology.node) {
+                // Check if node is a swicth or a host
+                // Termination points have themselves as a termination point
+                console.log(node);
+                console.log("------------");
+
+                // let nodeInfo = {};
+                retNodesInfo[node['node-id']] = {};
+                retNodesInfo[node['node-id']]["id"] = node['node-id'];
+
+                let color = 'green';
+                let svgIcon = pcSVG;
+                //let switchNames = new Set(); 
+                if (node['termination-point'][0]['tp-id'] !== node['node-id']) 
+                {
+                    color = 'red';
+                    svgIcon = switchSVG;
+                    retNodesInfo[node['node-id']]["type"] = "switch";
+                    // Save switch names
+                    //switchNames.add(node['node-id']);
+                }
+                else
+                {
+                    // To node["host-tracker-service:addresses"] einai array, pros to apron to evala na fernei to [0]
+                    retNodesInfo[node['node-id']]["type"] = "host";
+                    // nodeInfo[node['node-id']][0]["ip"] = node["host-tracker-service:addresses"].ip;
+                    // nodeInfo[node['node-id']][0]["mac"] = node["host-tracker-service:addresses"].mac;
+                    // Pio panw to [0] gt? Kanonika afou exei polla adresses prepei ontws na to kanoume etsi, 
+                    // mesa se for kai arxikopoiimeno omws
+                    retNodesInfo[node['node-id']]["ip"] = node["host-tracker-service:addresses"][0].ip;
+                    retNodesInfo[node['node-id']]["mac"] = node["host-tracker-service:addresses"][0].mac;
+                }
+
+                const currNode = {
+                    id: node['node-id'],
+                    color: color,
+                    svg: svgIcon,
+                }
+                retNodes.push(currNode);
+            }
+
+            // Then links
+            for (let link of topology.link) {
+                console.log(link);
+                console.log("=============");
+
+                const currLink = {
+                    source: link.source['source-node'],
+                    target: link.destination['dest-node'], 
+                }
+                retLinks.push(currLink);
+            }
+        } 
+
+        this.setState(
+            produce(draft => {
+                draft.graphNodes = retNodes;
+                draft.graphLinks = retLinks;
+                draft.nodesInfo = retNodesInfo;
+            })
+        );
+    }
+
+
+    //will be remove only for testing purposes
 
     testODLAPI = () => {
 
@@ -71,12 +172,25 @@ class CreateNetwork extends Component {
         });
     }
 
+    //--------------------------------
+
     render() {
         return (
             <Container fluid className={styles.MenuContainer}>
                 <Row>
                     <Col sm={6}>
-                        <Link to="/statistics" className={styles.MenuLink}>
+                        <Link 
+                            className={styles.MenuLink}
+                            to={{   
+                                    pathname: '/statistics', 
+                                    data: { 
+                                        graphNodes: this.state.graphNodes,
+                                        graphLinks: this.state.graphLinks,
+                                        nodesInfo: this.state.nodesInfo,
+                                        linksInfo: this.state.linksInfo
+                                    } 
+                                }} 
+                        >
                             <Jumbotron>
                                 <h1 className="display-5">Application 1</h1>
                                 <p 
@@ -116,29 +230,6 @@ class CreateNetwork extends Component {
                     {/* </Link> */}
                     </Col>
                 </Row>
-                {/* <Row>
-                    <Col sm={4}/>
-                    <Col sm={4}>
-                        <Link to="/" className={styles.MenuLink}>
-                            <Jumbotron>
-                                <h1 className="display-5">Application 1</h1>
-                                <p 
-                                    className="lead"
-                                >
-                                    This is a simple hero unit, a simple Jumbotron-style component 
-                                    for calling extra attention to featured content or information.
-                                </p>
-                                <hr className="my-2" />
-                                <p>
-                                    It uses utility classes for typography and spacing to space content 
-                                    out within the larger container.
-                                </p>
-                            </Jumbotron>
-                        </Link>
-                    </Col>
-                    <Col sm={4}/>
-                </Row> */}
-
 
             </Container>
         );
@@ -148,4 +239,4 @@ class CreateNetwork extends Component {
 
 
 
-export default CreateNetwork;
+export default withRouter(CreateNetwork);
--- net-man-app/src/containers/StatisticsApp/statisticsApp.js
+++ net-man-app/src/containers/StatisticsApp/statisticsApp.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Table, Spinner } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 
 //import styles from './statisticsApp.module.css';
 import { openDaylightApi } from '../../services/openDaylightApi';
 
 import TopologyGraph from '../TopologyGraph/topologyGraph';
 import produce from 'immer';
-import pcSVG from '../../assets/svg/pcIcon.svg';
-import switchSVG from '../../assets/svg/hub.svg';
+
 import { getWidth, getHeight } from '../../utilities/utilities';
 
 
@@ -16,105 +15,10 @@ class StatisticsApp extends Component {
 
     state = {
         selectedNodeId: null,
-        selectedLinkId: null,
-        graphNodes: null,
-        graphLinks: null,
-        nodesInfo: null,
-        linksInfo: null
+        selectedLinkId: null
     }
 
-    componentDidMount() {
-        // alert("did mount");
-        if (this.state.graphNodes)
-        { // graph data have already been retrieved
-            // alert("already retrieved")
-            return;
-        }
-
-        // alert("go to retrieve data")
-
-        openDaylightApi.getTopology()
-            .then(data => {
-                console.log('openDaylight data:');
-                console.log(data['network-topology'].topology);
-                
-                this.setGraphData(data['network-topology'].topology);
-            });
-    }
-
-    setGraphData = (statistics) => {
-        let retNodes = [];
-        let retLinks = [];
-        let retNodesInfo = {};
-
-        console.log("------>MAKING DATA<-----------");
-
-        // Can handle many topologies
-        for (let topology of statistics) {
-            // Nodes
-            for (let node of topology.node) {
-                // Check if node is a swicth or a host
-                // Termination points have themselves as a termination point
-                console.log(node);
-                console.log("------------");
-
-                // let nodeInfo = {};
-                retNodesInfo[node['node-id']] = {};
-                retNodesInfo[node['node-id']]["id"] = node['node-id'];
-
-                let color = 'green';
-                let svgIcon = pcSVG;
-                //let switchNames = new Set(); 
-                if (node['termination-point'][0]['tp-id'] !== node['node-id']) 
-                {
-                    color = 'red';
-                    svgIcon = switchSVG;
-                    retNodesInfo[node['node-id']]["type"] = "switch";
-                    // Save switch names
-                    //switchNames.add(node['node-id']);
-                }
-                else
-                {
-                    // To node["host-tracker-service:addresses"] einai array, pros to apron to evala na fernei to [0]
-                    retNodesInfo[node['node-id']]["type"] = "host";
-                    // nodeInfo[node['node-id']][0]["ip"] = node["host-tracker-service:addresses"].ip;
-                    // nodeInfo[node['node-id']][0]["mac"] = node["host-tracker-service:addresses"].mac;
-                    // Pio panw to [0] gt? Kanonika afou exei polla adresses prepei ontws na to kanoume etsi, 
-                    // mesa se for kai arxikopoiimeno omws
-                    retNodesInfo[node['node-id']]["ip"] = node["host-tracker-service:addresses"][0].ip;
-                    retNodesInfo[node['node-id']]["mac"] = node["host-tracker-service:addresses"][0].mac;
-                }
-
-                const currNode = {
-                    id: node['node-id'],
-                    color: color,
-                    svg: svgIcon,
-                }
-                retNodes.push(currNode);
-            }
-
-            // Then links
-            for (let link of topology.link) {
-                console.log(link);
-                console.log("=============");
-
-                const currLink = {
-                    source: link.source['source-node'],
-                    target: link.destination['dest-node'], 
-                }
-                retLinks.push(currLink);
-            }
-        } 
-
-        this.setState(
-            produce(draft => {
-                draft.graphNodes = retNodes;
-                draft.graphLinks = retLinks;
-                draft.nodesInfo = retNodesInfo;
-            })
-        );
-    }
-
+    
 
     nodeClickedHandler = (nodeId) => {
         // alert(`node clicked ${nodeId}`);
@@ -152,30 +56,16 @@ class StatisticsApp extends Component {
     render () {
 
         console.log("inside statistics app rendering");
-        console.log("node info: ", this.state.nodesInfo);
 
-        // alert("rendering app")
+        alert("rendering app")
+        // console.log(this.props.location.data.graphNodes);
         const graphWidth = getWidth() * 0.6;
         const graphHeight = getHeight() * 0.7;
 
-        // if (!this.state.graphNodes)
-        // {
-        //     alert("not going to render graph")
-        // }
-        // else
-        // {
-        //     alert("going to render graph")
-
-        // }
-
-
-        // console.log("graph Width: ", graphWidth);
-        // console.log("graph Height", graphHeight);
-
         return (
             <div>
-                {!this.state.graphNodes ?
-                    <p>is loading</p>
+                {!this.props.location.data ?
+                    <Redirect to="/"/>
                 :
 
                 <>
@@ -185,14 +75,14 @@ class StatisticsApp extends Component {
                             nodeClickedHandler={this.nodeClickedHandler}
                             linkClickedHandler={this.linkClickedHandler}
                             graphClickedHandler={this.graphClickedHandler}
-                            nodes={this.state.graphNodes}
-                            links={this.state.graphLinks}
+                            nodes={this.props.graphNodes}
+                            links={this.props.graphLinks}
                             graphWidth={graphWidth}
                             graphHeight={graphHeight}
                        />
                     </div>
 
-                    {this.state.selectedNodeId ?
+                    {this.state.selectedNodeId || this.state.selectedLinkId ?
                     <div className="border w-100 p-2">
                         <Container fluid>
 
@@ -210,7 +100,7 @@ class StatisticsApp extends Component {
                                 </Col>
 
                                 <Col sm="6">
-                                    {this.state.nodesInfo[this.state.selectedNodeId].type}
+                                    {this.props.nodesInfo[this.state.selectedNodeId].type}
                                 </Col>
                             </Row>
 
@@ -220,7 +110,7 @@ class StatisticsApp extends Component {
                                 </Col>
 
                                 <Col sm="6">
-                                    {this.state.nodesInfo[this.state.selectedNodeId].id}
+                                    {this.props.nodesInfo[this.state.selectedNodeId].id}
                                 </Col>
                             </Row>
 
@@ -230,7 +120,7 @@ class StatisticsApp extends Component {
                                 </Col>
 
                                 <Col sm="6">
-                                    {this.state.nodesInfo[this.state.selectedNodeId].ip}
+                                    {this.props.nodesInfo[this.state.selectedNodeId].ip}
                                 </Col>
                             </Row>
 
@@ -240,7 +130,7 @@ class StatisticsApp extends Component {
                                 </Col>
 
                                 <Col sm="6">
-                                    {this.state.nodesInfo[this.state.selectedNodeId].mac}
+                                    {this.props.nodesInfo[this.state.selectedNodeId].mac}
                                 </Col>
                             </Row>
 
@@ -292,4 +182,4 @@ class StatisticsApp extends Component {
 }
 
 
-export default StatisticsApp;
+export default withRouter(StatisticsApp);
