424df48e280aab64c8411f6c0ec91eca3b1df255
John Pap <sdi1400144@di.uoa.gr>
2019-08-16 00:02:50 +0300

    Made changes in StatisticsApp

--- net-man-app/src/containers/StatisticsApp/statisticsApp.js
+++ net-man-app/src/containers/StatisticsApp/statisticsApp.js
@@ -61,118 +61,50 @@ class StatisticsApp extends Component {
         const graphHeight = getHeight() * 0.7;
 
         return (
-            <div>
+            <>
                 {!this.props.location.data ?
                     <Redirect to="/"/>
                 :
 
-                <>
-                <div className="d-flex d-flex-row border">
-                    <div className="border">
-                        <TopologyGraph
-                            nodeClickedHandler={this.nodeClickedHandler}
-                            linkClickedHandler={this.linkClickedHandler}
-                            graphClickedHandler={this.graphClickedHandler}
-                            nodes={this.props.location.data.graphNodes}
-                            links={this.props.location.data.graphLinks}
-                            graphWidth={graphWidth}
-                            graphHeight={graphHeight}
-                       />
+                    <div style={{borderTop: "2px solid gray", borderLeft: "2px solid gray", borderRight: "2px solid gray"}}>
+
+                        <div className="d-flex d-flex-row justify-content-center" style={{backgroundColor: "GhostWhite"}}>
+                            <div className="font-weight-bold customHeader1">
+                                Topology Overview
+                            </div>
+                        </div>
+
+                        <div className="d-flex d-flex-row" style={{borderTop: "2px solid gray", borderBottom: "2px solid gray"}}>
+                            <div style={{borderRight: "2px solid gray"}}>
+                                <TopologyGraph
+                                    nodeClickedHandler={this.nodeClickedHandler}
+                                    linkClickedHandler={this.linkClickedHandler}
+                                    graphClickedHandler={this.graphClickedHandler}
+                                    nodes={this.props.location.data.graphNodes}
+                                    links={this.props.location.data.graphLinks}
+                                    graphWidth={graphWidth}
+                                    graphHeight={graphHeight}
+                            />
+                            </div>
+
+                            <div className="w-100 p-2" style={{backgroundColor: "GhostWhite"}}>
+                                {this.state.selectedNodeId || this.state.selectedLinkId ?
+                                    // <HostInfo/>
+                                    null
+                                : null
+                                }
+                            </div>
+                        </div>
+
+                        {this.state.selectedNodeId ?
+                            <div className="d-flex d-flex-row" style={{borderBottom: "2px solid gray", backgroundColor: "GhostWhite"}}>
+                                wswswswswsw
+                            </div>
+                            : null
+                        }
                     </div>
-
-                    {this.state.selectedNodeId || this.state.selectedLinkId ?
-                    <div className="border w-100 p-2">
-                        <Container fluid>
-
-                            <Row className="border">
-                                <Col sm="12" className="font-weight-bold border d-flex justify-content-center">
-                                    <div>
-                                        Information
-                                    </div>
-                                </Col>
-                            </Row>
-
-                            <Row className="border">
-                                <Col sm="6" className="font-weight-bold border">
-                                    Type
-                                </Col>
-
-                                <Col sm="6">
-                                    {this.props.location.data.nodesInfo[this.state.selectedNodeId].type}
-                                </Col>
-                            </Row>
-
-                            <Row className="border">
-                                <Col sm="6" className="font-weight-bold border">
-                                    Id
-                                </Col>
-
-                                <Col sm="6">
-                                    {this.props.location.data.nodesInfo[this.state.selectedNodeId].id}
-                                </Col>
-                            </Row>
-
-                            <Row className="border">
-                                <Col sm="6" className="font-weight-bold border">
-                                    IP
-                                </Col>
-
-                                <Col sm="6">
-                                    {this.props.location.data.nodesInfo[this.state.selectedNodeId].ip}
-                                </Col>
-                            </Row>
-
-                            <Row className="border">
-                                <Col sm="6" className="font-weight-bold border">
-                                    mac
-                                </Col>
-
-                                <Col sm="6">
-                                    {this.props.location.data.nodesInfo[this.state.selectedNodeId].mac}
-                                </Col>
-                            </Row>
-
-                            <Row className="border">
-                                <Col sm="6" className="font-weight-bold border">
-                                    Name
-                                </Col>
-
-                                <Col sm="6">
-                                    None
-                                </Col>
-                            </Row>
-
-                            <Row className="border">
-                                <Col sm="6" className="font-weight-bold border">
-                                    Connectors #
-                                </Col>
-
-                                <Col sm="6">
-                                    4
-                                </Col>
-                            </Row>
-
-                            <Row className="border mt-3">
-                                <Col sm="12" className="font-weight-bold border">
-                                    Attachment Points
-                                </Col>
-                            </Row>
-
-
-                        
-                        </Container>
-                    </div>
-                    : null
-                    }
-                </div>
-
-                <div className="d-flex d-flex-row border">
-                    wswswswswsw
-                </div>
-                </>
                 }
-
-            </div>
+            </>
         )
 
     }
