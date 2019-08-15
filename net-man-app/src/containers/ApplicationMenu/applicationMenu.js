271fef81813500a35f38a3877fdfef9ac3891fd8
John Pap <sdi1400144@di.uoa.gr>
2019-08-15 06:45:51 +0300

    Fixed graphNodes, nodesInfo extraction/creation in ApplicationMenu.js

--- net-man-app/src/containers/ApplicationMenu/applicationMenu.js
+++ net-man-app/src/containers/ApplicationMenu/applicationMenu.js
@@ -19,14 +19,12 @@ class CreateNetwork extends Component {
         graphNodes: null,
         graphLinks: null,
         nodesInfo: null,
-        linksInfo: null,
-        nodeConnectorData: null,
-        linkConcatToPort: null,
+        linksInfo: null
     }
 
 
     componentDidMount() {
-        // alert("did mount");
+        // alert("did mount"); peritto
         if (this.state.graphNodes)
         { // graph data have already been retrieved
             alert("already retrieved")
@@ -36,73 +34,110 @@ class CreateNetwork extends Component {
         // alert("go to retrieve data")
 
         openDaylightApi.getTopology()
-            .then(topData => {
-                
+            .then(topologyData => {
                 
                 openDaylightApi.getNodes()
-                    .then(data => {
-                        console.log('openDaylight node data:');
-                        console.log(data.nodes.node);
-                        console.log("----------------");
-
+                    .then(nodesData => {
+                        // console.log('++++++> openDaylight nodes data:');
+                        // console.log(nodesData.nodes.node);
+                        // console.log("----------------");
 
                         // data.nodes.node is the array of nodes
-                        this.setNodeConnectorData(data.nodes.node);
+                        // this.setNodeConnectorData(data.nodes.node);
 
-                        console.log("=========================");
+                        
+                        // console.log("=========================");
 
 
-
-
-                        console.log('openDaylight topology data:');
-                        console.log(topData['network-topology'].topology);
-                        console.log("----------------");
+                        // console.log('++++++> openDaylight topology data:');
+                        // console.log(topologyData['network-topology'].topology);
+                        // console.log("----------------");
                             
-                        this.setGraphData(topData['network-topology'].topology);
+                        // this.setGraphData(topologyData['network-topology'].topology);
 
-                        console.log("=========================");
+                        // console.log("=========================");
 
 
+
+                        this.setNodesDataSets(topologyData['network-topology'].topology[0].node, this.getNodesConnectorsData(nodesData.nodes.node));
                     });
-                
-                
-                
-                
-                
-                
-
-
-
-
-
             });
-
-
-        
     }
 
-    // this.state.nodeConnectorData[nodeConnector.id (dld linkid)] = statistics
-    // Genika statistcs pros to paron, mexri na ta emfanizoume
-    setNodeConnectorData = (nodes) => {
-        // Object vs Array? (same thing??)
-        //let retNodeConnectorData = [];
-        let retNodeConnectorData = {};
+    setNodesDataSets = (nodesTopo, nodesConnectors) => {
+        // nodesConnectors:
+        // nodesTopo: includes both hosts and switches info. 
+        // However, nodesTopo does not contain extensive info about switches.
+        // So, will use nodesAnalytics to extract hosts' info
 
-        // Nodes only contains switches
-        for (let node of nodes) { 
-            for (let connector of node['node-connector']) {
-                retNodeConnectorData[connector.id] = connector;
+        let retGraphNodes = [];
+        let retNodesInfo = {};
+        for (let nTopo of nodesTopo) 
+        { 
+            // {<node1_id> : {}, <node2_id> : {} ...}
+            const nodeId = nTopo['node-id'];
+            retNodesInfo[nodeId] = {};
+            retNodesInfo[nodeId]["id"] = nodeId;
+
+            let nodeSVGicon = null;
+
+            //check if node is host or switch
+            if (nTopo['termination-point'][0]['tp-id'] !== nodeId) 
+            {   //it is switch
+                nodeSVGicon = switchSVG;
+                retNodesInfo[nodeId]["type"] = "switch";
+
+                retNodesInfo[nodeId]["switchType"] = nodesConnectors[nodeId]["switchType"];
+                retNodesInfo[nodeId]["info"] = nodesConnectors[nodeId]["info"]; //isws peritto !!!!!!!!
+                retNodesInfo[nodeId]["connectors"] = nodesConnectors[nodeId]["connectors"];
             }
-        }
+            else
+            {   //it is host
+                nodeSVGicon = pcSVG;
 
-        console.log('node connectors');
-        console.log(retNodeConnectorData);
+                retNodesInfo[nodeId]["type"] = "host";
+                retNodesInfo[nodeId]["ip"] = nTopo["host-tracker-service:addresses"][0].ip;
+                retNodesInfo[nodeId]["mac"] = nTopo["host-tracker-service:addresses"][0].mac;
+            }
+
+            //isws na pros8esoume kai me poia nodes einai connected
+
+            const graphNode = {
+                id: nodeId,
+                svg: nodeSVGicon,
+            }
+            retGraphNodes.push(graphNode);
+        }
 
         this.setState(
             produce(draft => {
-                draft.nodeConnectorData = retNodeConnectorData;
+                draft.graphNodes = retGraphNodes;
+                draft.nodesInfo = retNodesInfo;
             })
         );
+    }
+
+    // this.state.nodeConnectorData[nodeConnector.id (dld linkid)] = statistics
+    getNodesConnectorsData = (nodesAnalytics) => {
+
+        let retNodesConnectorsData = {};
+
+        // nodesAnalytics only contains switches
+        for (let node of nodesAnalytics) 
+        { 
+            retNodesConnectorsData[node.id] = {};
+            retNodesConnectorsData[node.id]["switchType"] = node["flow-node-inventory:hardware"];
+
+            retNodesConnectorsData[node.id]["info"] = node; //isws peritto !!!!!!!!
+
+            retNodesConnectorsData[node.id]["connectors"] = {};
+            for (let connector of node['node-connector']) 
+            {
+                retNodesConnectorsData[node.id]["connectors"][connector.id] = connector;
+            }
+        }
+
+        return retNodesConnectorsData;
     }
 
 
@@ -156,23 +191,20 @@ class CreateNetwork extends Component {
                 }
                 retNodes.push(currNode);
 
-
-
-
             }
 
             // Then links
             let linkConcatToPort = {}
             for (let link of topology.link) {
-                console.log('linkInfo')
-                console.log(link);
-                console.log("=============");
+                // console.log('linkInfo')
+                // console.log(link);
+                // console.log("=============");
 
                 let linkSrc = link.source['source-node'];
                 let linkDest = link.destination['dest-node'];
                 
                 if (this.state.nodeConnectorData[link['link-id']]) {
-                    linkConcatToPort[linkSrc + '' + linkDest] = this.state.nodeConnectorData[link['link-id']]['flow-node-inventory:port-number'];
+                    linkConcatToPort[linkSrc + '/' + linkDest] = this.state.nodeConnectorData[link['link-id']]['flow-node-inventory:port-number'];
                 }
                 
 
@@ -184,10 +216,8 @@ class CreateNetwork extends Component {
                 retLinks.push(currLink);
             }
 
-
-
-            console.log('LINK CONCAT TO PORT ')
-            console.log(linkConcatToPort);
+            // console.log('LINK CONCAT TO PORT ')
+            // console.log(linkConcatToPort);
 
             this.setState(
                 produce(draft => {
@@ -197,8 +227,8 @@ class CreateNetwork extends Component {
 
         } 
 
-        console.log("nodes info: ");
-        console.log(retNodesInfo);
+        // console.log("nodes info: ");
+        // console.log(retNodesInfo);
 
         this.setState(
             produce(draft => {
@@ -208,8 +238,6 @@ class CreateNetwork extends Component {
             })
         );
 
-
-        
     }
 
 
@@ -270,6 +298,26 @@ class CreateNetwork extends Component {
     //--------------------------------
 
     render() {
+
+        console.log("---> Printing ODL Info <---");
+
+        console.log("--> Graph Nodes: ", this.state.graphNodes);
+        console.log("--------------------");
+
+        console.log("--> Nodes Info: ", this.state.nodesInfo);
+        console.log("--------------------");
+
+        console.log("--> LinksInfo: ", this.state.linksInfo )
+        console.log("--------------------");
+
+        // console.log("--> node Connector Data: ", this.state.nodeConnectorData)
+        // console.log("--------------------");
+
+        // console.log("--> links Concat to Port: ", this.state.linkConcatToPort)
+        // console.log("--------------------");
+        // console.log("======================================");
+
+
         return (
             <Container fluid className={styles.MenuContainer}>
 
